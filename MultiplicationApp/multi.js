7a749e036e3f7c12c2db6ae23a50bd59d7757fc6
syeda-sadia-banu <sadia.banu17@gmail.com>
2023-02-04 16:41:51 +0530

    modified

--- MultiplicationApp/multi.js
+++ MultiplicationApp/multi.js
@@ -6,8 +6,13 @@ const num2=Math.ceil(Math.random()*10);
 const questionEl=document.getElementById("question");
 const inputEl=document.querySelector(".text");
 const scoreEl=document.querySelector(".score");
-let score=0;
-scoreEl.innerText="score: "+ score;
+let score=JSON.parse(localStorage.getItem("score"));
+// scoreEl.innerText="score: "+ score;
+
+if(!score){
+    score=0;
+}
+scoreEl.innerText=`score:${score}`
 
 questionEl.innerText=`What is ${num1} multiply by ${num2}?`
 
@@ -19,11 +24,15 @@ formEl.addEventListener("submit", () => {
     const userAns = +inputEl.value;
     if (userAns === correctAns) {
       score++;
-     // updateLocalStorage();
-     scoreEl.innerText=`score:${score}`
+     updateLocalStorage();
+     document.querySelector(".result").innerText="correct ans"
+     
     } else {
       score--;
-      //updateLocalStorage();
+      updateLocalStorage();
+      document.querySelector(".result").innerText="Wrong ans"
     }
   });
-  console.log(score);
+  function updateLocalStorage() {
+    localStorage.setItem("score", JSON.stringify(score));
+  }
